c5c545d0daf46edb9a3797e4121d8682549bab4b
Yovanza054 <147380457+Yovanza054@users.noreply.github.com>
2023-11-03 22:54:08 +0700

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,11 @@
+public class Task1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i <= 9) {
+                System.out.println(i);
+            } else {
+                System.out.println("Yovanza Villareal");
+            }
+        }
+    }
+}
